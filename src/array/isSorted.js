ed7572f7232e22ac9e1887218131d408bd291486
tangweikun <819573105@qq.com>
2018-03-03 17:33:40 +0800

    :sparkles: isSorted

---
+++ src/array/isSorted.js
@@ -0,0 +1,15 @@
+// FIXME:
+
+export const isSorted = arr => {
+  let pre = arr[0]
+  let direction = undefined
+  for (let i = 1; i < arr.length; i++) {
+    if (direction * (pre - arr[i]) > 0) return 0
+    if (direction === undefined) {
+      if (pre > arr[i]) direction = -1
+      if (pre < arr[i]) direction = 1
+    }
+    pre = arr[i]
+  }
+  return direction
+}
