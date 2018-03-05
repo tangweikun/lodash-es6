8322c3fc5f4db7f1f290f5578f5619b29ce60b49
tangweikun <819573105@qq.com>
2018-03-05 13:02:31 +0800

    :hammer: Refactor sortedIndex

--- src/array/sortedIndex.js
+++ src/array/sortedIndex.js
@@ -1,16 +1,4 @@
 export const sortedIndex = (arr, n) => {
-  const order = getOrder(arr)
-  return order === 0 ? 0 : arr.findIndex(x => order * (x - n) >= 0)
-}
-
-const getOrder = arr => {
-  let i = 1
-
-  while (i < arr.length) {
-    if (arr[i] > arr[i - 1]) return 1
-    if (arr[i] < arr[i - 1]) return -1
-    i++
-  }
-
-  return 0
+  const difference = arr.length <= 1 ? 0 : arr[arr.length - 1] - arr[0]
+  return difference === 0 ? 0 : arr.findIndex(x => difference * (x - n) >= 0)
 }
