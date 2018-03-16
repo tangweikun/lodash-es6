b8106b8cc81751ef6e33f8e6b91cb2b71f020440
tangweikun <819573105@qq.com>
2018-03-16 10:57:02 +0800

    :memo: Update TODO

--- src/array/pullAt.js
+++ src/array/pullAt.js
@@ -1,4 +1,5 @@
 // Removes elements from array corresponding to indexes and returns an array of removed elements.
+// indexes should be sorted by ascending order and be unique
 
 export const pullAt = (arr, indexes) =>
   indexes.reduce((acc, x, i) => [...acc, ...arr.splice(x - i, 1)], [])
