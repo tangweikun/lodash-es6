5f11fc28d723087384ed86a04d9fbebd9a86fd48
tangweikun <819573105@qq.com>
2018-03-16 18:50:28 +0800

    :hammer: Refactor zip

--- src/array/zip.js
+++ src/array/zip.js
@@ -2,20 +2,25 @@
 // the first of which contains the first elements of the given arrays,
 // the second of which contains the second elements of the given arrays, and so on.
 // TODO:
-export const zip = (...arr) => {
-  let maxLength = 0
-  let res = []
-  for (let el of arr) {
-    maxLength = Math.max(maxLength, el.length)
-  }
+// export const zip = (...arr) => {
+//   let maxLength = 0
+//   let res = []
+//   for (let el of arr) {
+//     maxLength = Math.max(maxLength, el.length)
+//   }
 
-  for (let j = 0; j < maxLength; j++) {
-    const foo = []
-    for (let n of arr) {
-      foo.push(n[j])
-    }
-    res.push(foo)
-  }
+//   for (let j = 0; j < maxLength; j++) {
+//     const foo = []
+//     for (let n of arr) {
+//       foo.push(n[j])
+//     }
+//     res.push(foo)
+//   }
 
-  return res
-}
+//   return res
+// }
+
+export const zip = (...args) =>
+  [...Array(Math.max(...args.map(arr => arr.length)))].map((x, i) =>
+    args.map(y => y[i]),
+  )
