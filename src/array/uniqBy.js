bab01540b901339be48ca467b57d241c25846a9d
tangweikun <819573105@qq.com>
2018-03-16 15:02:19 +0800

    :hammer: Refactor uniqBy

--- src/array/uniqBy.js
+++ src/array/uniqBy.js
@@ -3,15 +3,22 @@
 // The order of result values is determined by the order they occur in the array.
 
 // TODO: Make code less
-export const uniqBy = (arr, fn) => {
-  let res = []
-  const hash = new Set()
-  for (let i = 0; i < arr.length; i++) {
-    const foo = typeof fn === 'function' ? fn(arr[i]) : arr[i][fn]
-    if (!hash.has(foo)) {
-      res.push(arr[i])
-      hash.add(foo)
-    }
-  }
-  return res
-}
+// export const uniqBy = (arr, fn) => {
+//   let res = []
+//   const hash = new Set()
+//   for (let i = 0; i < arr.length; i++) {
+//     const foo = typeof fn === 'function' ? fn(arr[i]) : arr[i][fn]
+//     if (!hash.has(foo)) {
+//       res.push(arr[i])
+//       hash.add(foo)
+//     }
+//   }
+//   return res
+// }
+
+export const uniqBy = (arr, fn, set = new Set()) =>
+  arr.filter(el =>
+    (v => !set.has(v) && set.add(v))(
+      typeof fn === 'function' ? fn(el) : el[fn],
+    ),
+  )
